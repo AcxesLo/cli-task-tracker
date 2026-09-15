4e1a95e75c76851c3d4d1a03ffb5eb6c3a348102
AcxesLo <steve.klein.98@web.de>
2026-09-15 15:20:19 +0200

    updated TODO inside Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,10 +6,6 @@ import task.TaskService;
 public class Main {
 
     //TODO
-    // create enum for task status
-    // AtomicInteger for task-id
-    // HashMap to save up the tasks
-    // from if to switch statements
 
     static void main(String[] args) {
 
